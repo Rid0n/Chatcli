2aa0f5bac4ebe3e781a007db361d550017ad9a69
Rid0n <47979401+Rid0n@users.noreply.github.com>
2020-06-26 22:48:14 +0300

    added preliminary docs

--- Chatcli/Program.cs
+++ Chatcli/Program.cs
@@ -6,6 +6,10 @@ namespace Chatcli
     {
         public bool chooseServerOrClient()
         {
+            Console.WriteLine("Choose startup mode: client/server");
+            string mode = null;
+
+            return mode=="s";
 
         }
         static void Main(string[] args)
