d9d2662309dd045e204b7ad1cb5d56fa02d8ea15
Rid0n <47979401+Rid0n@users.noreply.github.com>
2020-06-27 13:12:44 +0300

    internal logic added

--- Chatcli/Program.cs
+++ Chatcli/Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Chatcli
 {
+    [System.Runtime.InteropServices.ComVisible(true)]
     class Program
     {
-        public bool chooseServerOrClient()
-        {
-            Console.WriteLine("Choose startup mode: client/server");
-            string mode = null;
-
-            return mode=="s";
-
-        }
         static void Main(string[] args)
         {
+            //Console.WriteLine("Type 'S' to be the server, any other key to be a pleb");
+            //if (Console.ReadKey(true).Key == ConsoleKey.S); //we do partay
+            //else {
+                Console.WriteLine("choose a username: ");
+            //while (true) Console.Read()
+                UserConsole userConsole = new UserConsole(Console.ReadLine());
+                userConsole.Begin();
             
+           // }
         }
+        //KeyPressEventHandler()
+        
+        
     }
 }
